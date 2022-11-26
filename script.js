a29690d1066f7abe760a0655c9ab30b90eacce80
jayants345 <77769288+jayants345@users.noreply.github.com>
2022-11-26 10:55:30 +0530

    upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<FontAwesomeIcon icon="fa-thin fa-cart-shopping" />
+let loginForm = document.querySelector('.login-form-container');
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+}
